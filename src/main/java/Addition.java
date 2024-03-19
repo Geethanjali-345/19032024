9f7d5c9a3fb77265b4e497cfae9f3765da43546b
QualiZeal <geeth190903@gmail.com>
2024-03-19 13:09:13 +0530

    Submitting the code.

---
+++ src/main/java/Addition.java
@@ -0,0 +1,13 @@
+package com.qualizeal.javaapp.mathematics;
+public class Addition {
+		public void add() {
+			double num1=13.05d;
+			double num2=12.05d;
+			double num3=25.00d;
+			double num4=50.00d;
+			double result=num1+num2+num3+num4;
+			System.out.println("Sum of given four numbers of double type : "+result);
+
+	               }
+	            }
+
---
+++ src/main/java/Program.java
@@ -0,0 +1,9 @@
+package com.qualizeal.javaapp.mainprogram;
+import com.qualizeal.javaapp.mathematics.Addition;
+
+public class Program {
+	public static void main(String[] args) {
+	Addition addition=new Addition();
+	addition.add();
+       }
+}
